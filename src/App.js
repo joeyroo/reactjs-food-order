8069ae216be6c22ee024d653b24cc450e23a6f42
Josef Roux <josef@roux.me.uk>
2021-08-31 22:29:27 +0100

    Corrected import conflict

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import CartProvider from './store/CartProvider'
-=======
-import Header from './components/Layout/Header'
-import Meals from './components/Meals/Meals'
-import CartProvider from './store/CartProvider'
 import Cart from './components/Cart/Cart'
->>>>>>> feat/cart
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -23,7 +17,7 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
